e87664ee7c7291b1932dd369416b26f89fdc3c60
ROSTAND LELE <rostoinfocus@gmail.com>
2024-11-09 06:43:12 +0100

    Algorithme : Saisie et Affichage Inversé
    Algorithme : Saisie et Affichage Inversé

---
+++ exo-1.algo.c
@@ -0,0 +1,14 @@
+Voici un algorithme pour saisir 100 nombres réels et les afficher dans l'ordre inverse de leur saisie :
+
+### Algorithme : Saisie et Affichage Inversé
+
+**Variables** :
+- \( \text{Tab} \) : Tableau de réels de taille 100
+- \( i \) : Entier pour l'index de la boucle
+
+**Début** :
+1. Pour \( i \) allant de 0 à 99 faire :
+   - Lire \( \text{Tab}[i] \) // Saisir le \( i \)-ème réel
+2. Pour \( i \) allant de 99 à 0 faire :
+   - Afficher \( \text{Tab}[i] \) // Afficher le \( i \)-ème réel dans l'ordre inverse
+**Fin de l'algorithme**
